f6e53e00b0267be12653e3f1ba57bedb4542d64f
Marc Anton Dahmen <m.dahmen@herzogdemeuron.com>
2020-05-14 16:48:42 +0200

    Update startup.py

--- startup.py
+++ startup.py
@@ -1,1 +1,18 @@
-print('Jarvis core loaded')
+import constants
+import update
+import os
+from pyrevit import script
+from pyrevit import output
+
+out = script.get_output()
+pwd = os.path.dirname(__file__)
+update = update.jarvisUpdater()
+
+output.set_stylesheet(pwd + '\\outputstyles.css')
+out.set_height(900)
+out.set_width(500)
+out.print_image('{}/jarvis-core.svg'.format(pwd))
+out.center()
+
+update.jarvis(constants.JRVS_DIR)
+update.extensions(constants.JRVS_EXTENSIONS_DIR)
