95ba2b3b96d9e05f8b460a046fc17a418e5ebc77
Marc Anton Dahmen <revitron@marcdahmen.de>
2020-05-26 14:19:10 +0200

    Update ui.py

--- jarvis/system/ui.py
+++ jarvis/system/ui.py
@@ -2,15 +2,15 @@ import os
 import sys
 from pyrevit import script
 from pyrevit import output
-import constants
+import jarvis
 
 class UI:
         
     @staticmethod 
     def printLogo():
         out = script.get_output()
-        out.print_image(constants.JRVS_DIR + '/svg/jarvis-dark.svg')
+        out.print_image(jarvis.JRVS_DIR + '/svg/jarvis-dark.svg')
         
     @staticmethod  
     def setStyle():
-        output.set_stylesheet(constants.JRVS_CORE_DIR + '\\output.css')
+        output.set_stylesheet(jarvis.JRVS_CORE_DIR + '\\output.css')
