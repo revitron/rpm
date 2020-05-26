545002f1a9da8e01307e9c00c47d36be2bc26cab
Marc Anton Dahmen <revitron@marcdahmen.de>
2020-05-26 14:18:56 +0200

    Update __init__.py

--- jarvis/__init__.py
+++ jarvis/__init__.py
@@ -1,1 +1,6 @@
 from system import *
+
+JRVS_DIR = 'C:\\HdM-DT\\jarvis'
+JRVS_PYREVIT_BIN = JRVS_DIR + '\\jarvis-pyrevit\\bin\\pyrevit'
+JRVS_EXTENSIONS_DIR = JRVS_DIR + '\\jarvis-pyrevit\\extensions'
+JRVS_CORE_DIR = JRVS_EXTENSIONS_DIR + '\\jarvis-core.lib'
