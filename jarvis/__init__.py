6e60f2677f24abd833e88e693d8e99f65f2df748
Marc Anton Dahmen <revitron@marcdahmen.de>
2020-05-30 15:15:30 +0200

    Update __init__.py

--- jarvis/__init__.py
+++ jarvis/__init__.py
@@ -1,14 +1,15 @@
 import sys
+import os
 
-JRVS_DIR = 'C:\\HdM-DT\\jarvis'
-JRVS_PYREVIT_BIN = JRVS_DIR + '\\jarvis-pyrevit\\bin\\pyrevit'
-JRVS_EXTENSIONS_DIR = JRVS_DIR + '\\jarvis-pyrevit\\extensions'
+JRVS_PYREVIT_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
+JRVS_PYREVIT_BIN = JRVS_PYREVIT_DIR + '\\bin\\pyrevit'
+JRVS_EXTENSIONS_DIR = JRVS_PYREVIT_DIR + '\\jarvis-pyrevit\\extensions'
 JRVS_CORE_DIR = JRVS_EXTENSIONS_DIR + '\\jarvis-core.lib'
 JRVS_CORE_EXTENSIONS = [
     'revitron.lib',
     'revitron-ui.extension',
-    'jarvis-core.lib',
-    'jarvis-system.extension'
+    'jarvis.lib',
+    'jarvis-ui.extension'
 ]
 
 sys.path.append(JRVS_EXTENSIONS_DIR + '\\revitron.lib')
