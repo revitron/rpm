0bab4adce7947c0162682ab2c0c2dae3bf944ee0
Marc Anton Dahmen <revitron@marcdahmen.de>
2020-10-02 14:55:21 +0200

    Update Update Extensions_script.py

--- Revitron.tab/RPM.panel/Setup.pulldown/Update Extensions.pushbutton/Update Extensions_script.py
+++ Revitron.tab/RPM.panel/Setup.pulldown/Update Extensions.pushbutton/Update Extensions_script.py
@@ -1,4 +1,9 @@
 import revitron
+import os
+import sys 
+
+parent = os.path.dirname
+sys.path.append(parent(parent(parent(parent(parent(__file__))))))
 import rpm
 
 if not revitron.Document().isFamily():
