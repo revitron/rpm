c339d779b09ac4fe71c9ca6f03391a61ff0ecc6b
Marc Anton Dahmen <revitron@marcdahmen.de>
2020-10-02 15:00:10 +0200

    Update ui.py

--- rpm/system/ui.py
+++ rpm/system/ui.py
@@ -1,17 +1,10 @@
-import os
-import sys
 from pyrevit import script
-from pyrevit import output
 from rpm import config
-import revitron
+
 
 class UI:
         
     @staticmethod 
     def printLogo():
         out = script.get_output()
-        out.print_image(revitron.LIB_DIR + '/svg/revitron-white.svg')
-        
-    @staticmethod  
-    def setStyle():
-        output.set_stylesheet(revitron.LIB_DIR + '\\css\\output.css')
+        out.print_image(config.RPM_DIR + '/svg/rpm-readme.svg')
