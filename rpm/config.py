e8e25e39a06ffd0f9310c526e71c8ef9946c7c6c
Marc Anton Dahmen <revitron@marcdahmen.de>
2020-10-02 14:59:53 +0200

    Update config.py

--- rpm/config.py
+++ rpm/config.py
@@ -1,15 +1,13 @@
-import sys
+import pyrevit
 import os
 
-RPM_PYREVIT_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
-RPM_PYREVIT_BIN = RPM_PYREVIT_DIR + '\\bin\\pyrevit'
+RPM_DIR = os.path.dirname(os.path.dirname(__file__))
+RPM_PYREVIT_DIR = pyrevit.HOME_DIR
+RPM_PYREVIT_BIN = pyrevit.BIN_DIR + '\\pyrevit'
 RPM_EXTENSIONS_DIR = RPM_PYREVIT_DIR + '\\extensions'
 RPM_CORE_EXTENSIONS = [
     'revitron.lib',
     'revitron-ui.extension',
     'revitron-tests.extension',
-    'rpm.lib',
-    'rpm-ui.extension'
+    'rpm.extension'
 ]
-
-sys.path.append(RPM_EXTENSIONS_DIR + '\\revitron.lib')
