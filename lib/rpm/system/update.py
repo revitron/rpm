bdef781b3a4d155b8e397c7b555561816ed86fca
Marc Anton Dahmen <revitron@marcdahmen.de>
2020-10-05 13:55:47 +0200

    Fixed crash when remote repo was deleted

--- lib/rpm/system/update.py
+++ lib/rpm/system/update.py
@@ -9,12 +9,15 @@ class Update:
 		
 	@staticmethod
 	def check(repo):  
+		out = script.get_output()
 		try:
 			if not os.path.isdir(repo + '\\.git'):
 				return False
+			if not Update.remoteExists(repo):
+				out.print_html('<br>Error: Remote of repository "{}" not found!'.format(os.path.basename(repo)))
+				return False
 			status = Update.git('fetch origin --dry-run', repo)
 			if status:
-				out = script.get_output()
 				out.print_html('<br><b>{}</b> &mdash; updates available'.format(os.path.basename(repo)))			
 				print(status)
 				return True
@@ -35,7 +38,10 @@ class Update:
 		
 	@staticmethod
 	def git(cmd, repo):
-		return subprocess.check_output('git --git-dir={0}\\.git --work-tree={0} {1}'.format(repo, cmd), stderr=subprocess.STDOUT, shell=True, cwd='C:\\')
+		return subprocess.check_output('set GIT_TERMINAL_PROMPT=0 && git -c credential.helper= --git-dir={0}\\.git --work-tree={0} {1}'.format(repo, cmd), 
+                                 	   stderr=subprocess.STDOUT, 
+                                       shell=True, 
+                                       cwd='C:\\')
 		
 	@staticmethod
 	def getExtensionRepos():
@@ -50,6 +56,9 @@ class Update:
 	
 	@staticmethod 
 	def extension(repo):
+		if not Update.remoteExists(repo):
+			print('Error: Remote of repository "{}" not found!'.format(os.path.basename(repo)))
+			return False
 		status = Update.git('status --untracked-files=no --porcelain', repo)
 		if status:
 			print('Skipped update &mdash; repository not clean!')
@@ -60,7 +69,14 @@ class Update:
 	def extensions():
 		out = script.get_output()
 		for repo in Update.getExtensionRepos():
-			out.print_html('<br>')
-			out.print_html('<b>{}</b> &mdash; updating ...'.format(os.path.basename(repo)))
+			out.print_html('<br><b>{}</b> &mdash; updating ...'.format(os.path.basename(repo)))
 			Update.extension(repo)
-		
+   
+	@staticmethod
+	def remoteExists(repo):
+		url = Update.git('remote get-url --all origin', repo).rstrip()
+		try:
+			Update.git('ls-remote {}'.format(url), repo)
+			return True
+		except:
+			return False
